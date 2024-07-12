7e070f06ff78649f4b16d8bb26c70d42b31cfed2
Churilov Evgeny <i@churilovevgeny.ru>
2024-07-12 18:17:58 +0300

    Init

---
+++ eservice/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from eservice.apps import EserviceConfig
+
+app_name = EserviceConfig.name
+
+urlpatterns = [
+    # path('blog_form/', BlogCreateView.as_view(), name='blog_create'),
+]
